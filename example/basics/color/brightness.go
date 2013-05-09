5d59be7e060fc3021173a91d2d753e04b53ee307
meng-yang, Lee <crazylion2@gmail.com>
2013-05-09 10:29:34 +0800

    change translateTo => anyTo(uint8,float64)

--- example/basics/color/brightness.go
+++ example/basics/color/brightness.go
@@ -10,36 +10,34 @@
  * porting by crazylion
  */
 package main
-
 import (
-	"../../../goframework"
-	"fmt"
+    "fmt"
+    "../../../goframework"
 )
-
-var width, height int = 200, 200
-var barWidth int = 5
+var width,height int = 200,200
+var barWidth int =5
 var lastBar int = -1
-var coswave []float64 = make([]float64, width)
-
-func Draw() {
-	whichBar := int(goframework.MouseX() / barWidth)
-	if whichBar != lastBar {
-		barX := whichBar * barWidth
-		goframework.Fill(barX, 100, goframework.MouseY())
-		goframework.Rect(barX, 0, barWidth, height)
-		lastBar = whichBar
-	}
+var coswave []float64 =make([]float64,width )
+func Draw(){
+    goframework.StrokeWeight(0)
+    whichBar  := int(goframework.MouseX() / barWidth)
+  if whichBar != lastBar {
+      barX := whichBar * barWidth;
+    goframework.Fill(barX, 100, goframework.MouseY())
+    goframework.Rect(barX, 0, barWidth, height);
+    lastBar = whichBar
+  }
 }
 
-func Setup() {
-	goframework.FrameRate(60)
-	goframework.ColorMode(goframework.HSB)
-	goframework.Background(0)
+func Setup(){
+    goframework.FrameRate(60)
+    goframework.ColorMode(goframework.HSB)
+    goframework.Background(0)
 
 }
-func main() {
-	fmt.Println("start")
-	goframework.Setup(Setup)
-	goframework.Draw(Draw)
-	goframework.Win(width, height)
+func main(){
+    fmt.Println("start")
+    goframework.Setup(Setup)
+    goframework.Draw(Draw)
+    goframework.Win(width,height)
 }
